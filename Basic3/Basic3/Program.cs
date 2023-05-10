bc0d916c0e658c5db9db20c04825abd3d88ce6c7
JoongseokPark <112610049+JoongseokPark@users.noreply.github.com>
2023-05-10 18:25:47 +0900

    Add files via upload

---
+++ Basic3/Basic3/Program.cs
@@ -0,0 +1,52 @@
+﻿using Basic3;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("학생 이름 입력");
+            string name = Console.ReadLine() ?? string.Empty;
+            Student student = new Student(name);
+
+            student.Display_Name();
+            student.Name = "JS";
+            Console.WriteLine(student.Name);
+            //Console.WriteLine("학생 나이 입력");
+            //int age = int.Parse(Console.ReadLine() ?? string.Empty);
+            //student.Student_Name(name);
+            //student.Student_Age(age);
+
+            //int x = 10;
+            //int y = 45;
+            ////Program pg = new(); //Program 클래스 pg 생성
+            ////int result = pg.Add(x, y);
+            //int result = Add(x, y);
+            //Console.WriteLine(result);
+            //result = Sub(x, y);
+            //Console.WriteLine(result);
+            //result = Mul(x, y);
+            //Console.WriteLine(result);
+            //result = Div(x, y);
+            //Console.WriteLine(result);
+        }
+        //private static int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
+        //private static int Sub(int a, int b)
+        //{
+        //    return a - b;
+        //}
+        //private static int Mul(int a, int b)
+        //{
+        //    return a * b;
+        //}
+        //private static int Div(int a, int b)
+        //{
+        //    return a / b;
+        //}
+    }
+    
+}
---
+++ Basic3/Basic3/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic3
+{
+    internal class Student //internal은 같은 namespace에서 public
+    {
+
+        string _name;
+
+        public string Name { get; set; } //자동생성
+        //public string Name //properties 수동생성
+        //{
+        //    get
+        //    {
+        //        return _name;
+        //    }
+        //    set
+        //    {
+        //        _name = value;
+        //    }
+        //}
+        public Student(string name) {
+            _name = name;   
+        }
+
+        //public void SetName(string name) { _name = name; } //게터와 세터
+        //public string GetName() { return _name; }
+        public void Display_Name()
+        {
+            Console.WriteLine($"학생의 이름 : {_name}");
+        }
+        public void Student_Age(int age)
+        {
+            Console.WriteLine($"학생의 이름 : {age}");
+        }
+    }
+}
